235f9a648438ab608caffa5646e5fa6da2b40fae
blockodyssey-sckim <fpel8800@gmail.com>
2022-09-26 05:30:02 +0900

    add: plus, minus 버튼의 disabled 처리 관련 구현
    See Also: 4648525f18450094ff8cdcf228e953c0d9e929bd

--- src/App.js
+++ src/App.js
@@ -3,30 +3,43 @@ import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h3 data-testid='counter'>{counter}</h3>
-        <button
-          data-testid='minus-button'
-          onClick={() => {
-            setCounter((count) => count - 1);
-          }}
-        >
-          -
-        </button>
-        <button
-          data-testid='plus-button'
-          onClick={() => {
-            setCounter((count) => count + 1);
-          }}
-        >
-          +
-        </button>
-        <button data-testid='on/off-button' style={{ backgroundColor: "blue" }}>
-          on/off
-        </button>
+        <div>
+          <button
+            data-testid='minus-button'
+            onClick={() => {
+              setCounter((count) => count - 1);
+            }}
+            disabled={disabled}
+          >
+            -
+          </button>
+          <button
+            data-testid='plus-button'
+            onClick={() => {
+              setCounter((count) => count + 1);
+            }}
+            disabled={disabled}
+          >
+            +
+          </button>
+        </div>
+        <div>
+          <button
+            data-testid='on/off-button'
+            style={{ backgroundColor: "blue" }}
+            onClick={() => {
+              setDisabled((prev) => !prev);
+            }}
+          >
+            on/off
+          </button>
+        </div>
       </header>
     </div>
   );
